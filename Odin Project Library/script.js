5f5fff0efc30e00e031741eba20d400b58b4f456
Johnny Zhou <jonathanqyz@gmail.com>
2022-01-28 21:59:27 -0500

    Added stylization and user interface

--- Odin Project Library/script.js
+++ Odin Project Library/script.js
@@ -9,10 +9,43 @@ function Book(title, author, pages, read) {
   }
 }
 
+let myLibrary = []
+
+var bookBtn = document.getElementById('addBookBtn')
+var bookContainer = document.getElementById('booksGrid')
+var bookSubmitBtn = document.getElementById("submitBook")
+var bookName = document.getElementById('title')
+var bookAuthor = document.getElementById('author')
+var bookPages = document.getElementById('pages')
+var bookRead = document.getElementById('read')
+var bookUnread = document.getElementById('unread')
+var bookNum = 0;
+
+//adding a book to the library 
+bookBtn.addEventListener('click', () => displayPrompt())
+bookSubmitBtn.addEventListener('click', () => createNewBook())
+
+function displayPrompt() {
+  const prompt = document.createElement('div')
+  prompt.className = 'bookPrompt'
+  // const userInput = new Book(prompt('Title?'), prompt('Author?'), prompt('Pages?'));
+  //   // add book object to myLibrary and display it
+  //   myLibrary.addBookToLibrary(userInput);
+  //   updateLibrary();
+    console.log("this button has been clicked")
+    bookContainer.classList.add('active')
+}
+
+function createNewBook() {
+  var newBook = new Book(bookName.value, bookAuthor.value, bookPages.value, 'unread')
+  addBookToLibrary(newBook)
+}
+
 function addBookToLibrary(newBook) {
   if(!this.isInLibary(newBook)){
     this.push(newBook)
   }
+  displayLibrary()
 }
 
 
@@ -21,19 +54,12 @@ function isInLibary(newBook) {
 }
 
 function displayLibrary() {
-  var booksString = ''
-  this.array.forEach(element => {
-    booksString += Object.values(person) 
-  })
-  document.getElementById(booksGrid).innerHTML = booksString
+  const bookShelf = document.createElement('p')
+  bookShelf.innerHTML = "check"
+  bookShelf.insertBefore(bookContainer)
 }
 
-let myLibrary = []
 
-//example
-var book1 = new Book("Animal Farm", "Someone", 120, true)
-var book2 = new Book("Scarlet Letter", "Someone 2", 150, false)
-myLibrary.addBookToLibrary(book1)
-myLibrary.addBookToLibrary(book2)
 
-myLibrary.displayLibrary()
+
+
